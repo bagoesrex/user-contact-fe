ffad87530e28ed29b901f36f01f9837e0de2cfca
bagoesrex <bagoesrex@gmail.com>
2025-10-22 08:08:28 +0700

    feat(contact): implement fetch API route by id

--- app/api/contacts/[contactId]/route.ts
+++ app/api/contacts/[contactId]/route.ts
@@ -1,8 +1,8 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function DELETE(_: Request, { params }: { params: { contactId: string } }) {
-    const { contactId } = params
+export async function DELETE(_: Request, { params }: { params: Promise<{ contactId: string }> }) {
+    const { contactId } = await params;
     const token = (await cookies()).get("auth-token")?.value;
 
     if (!token) {
@@ -39,8 +39,8 @@ export async function DELETE(_: Request, { params }: { params: { contactId: stri
     }
 }
 
-export async function PUT(req: Request, { params }: { params: { contactId: string } }) {
-    const { contactId } = params
+export async function PUT(req: Request, { params }: { params: Promise<{ contactId: string }> }) {
+    const { contactId } = await params;
     const token = (await cookies()).get("auth-token")?.value;
 
     if (!token) {
@@ -72,6 +72,56 @@ export async function PUT(req: Request, { params }: { params: { contactId: strin
             data: json.data,
             message: "Berhasil mengupdate contact",
         });
+
+        return response;
+    } catch (err) {
+        console.error(err)
+        return NextResponse.json({ message: "Server error" }, { status: 500 });
+    }
+}
+
+export async function GET(_: Request, { params }: { params: Promise<{ contactId: string }> }) {
+    const { contactId } = await params;
+
+    const token = (await cookies()).get("auth-token")?.value;
+
+    if (!token) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+
+    try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/contacts/${contactId}`, {
+            method: "GET",
+            headers: {
+                Authorization: token,
+            },
+        });
+
+        if (res.status === 404) {
+            return NextResponse.json({ message: "Contact not found" }, { status: 404 });
+        }
+
+        const json = await res.json();
+
+        if (!res.ok) {
+            return NextResponse.json(
+                { message: json.message || "Gagal melihat detail contact" },
+                { status: res.status }
+            );
+        }
+
+        const data = json.data;
+
+        const response = NextResponse.json({
+            contact: {
+                id: data.id,
+                first_name: data.first_name,
+                last_name: data.last_name,
+                email: data.email,
+                phone: data.phone
+            },
+            message: "Berhasil mendapatkan detail contact"
+        })
 
         return response;
     } catch (err) {
